448d91ab1505b33d4cb4c2a9c6a2600895473b0a
Vytautas <vmikalainis@gmail.com>
2020-04-23 10:18:42 +0300

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id : 11, name: 'Item 1' },
+    {id : 12, name: 'Item 2' },
+    {id : 13, name: 'Item 3' },
+    {id : 14, name: 'Item 4' },
+    {id : 15, name: 'Item 5' },
+    {id : 16, name: 'Item 6' },
+    {id : 17, name: 'Item 7' },
+    {id : 18, name: 'Item 8' },
+    {id : 19, name: 'Item 9' },
+    {id : 20, name: 'Item 10' },
+
+];
